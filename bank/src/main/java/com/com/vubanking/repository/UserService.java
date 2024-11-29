007e139df241247dcddf90fc06bab0c2fa6a0793
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-11-29 13:39:36 +0300

    service 1

---
+++ bank/src/main/java/com/com/vubanking/repository/UserService.java
@@ -0,0 +1,20 @@
+package com.vubanking.service;
+
+import com.vubanking.model.User;
+import com.vubanking.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public User getUserByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+}
